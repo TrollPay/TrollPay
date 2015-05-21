ce43874b0e5fba8da17902362454b59460adb86c
Pavan Ravipati <psravipati@gmail.com>
2015-05-21 13:51:54 -0700

    Initial commit for server side syncing with venmo api

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 
 var express = require('express');
 var path = require('path');
+var api = require('./config.js');
 
 /* ***********************
  * Initialize Middleware *
@@ -13,16 +14,32 @@ var path = require('path');
 var app = express();
 
 // Use the static assets from the client directory
-app.use(express.static(path.resolve("./client")));
+// app.use(express.static(path.resolve("./client")));
+
 
 /* **************
  * GET Requests *
  * **************/
 
 // index.html
-app.get('/', function(req, res) {
-  res.sendFile('index.html');
+app.get('/pay', function(req, res) {
+  var url = "https://api.venmo.com/v1/oauth/authorize?client_id="+api.id+"&scope=make_payments%20access_profile%20access_email%20access_phone%20access_balance&response_type=code";
+  res.redirect(url);
 });
+
+app.get('/', function(req, res) {
+  if (req.url.search('code') > -1) {
+    var auth_code = req.url.substr(7);
+
+    var url = "https://api.venmo.com/v1/oauth/access_token";
+    res.send('<script>alert("Works")</script>');
+  }
+  else {
+    console.log(req.url);
+    res.send("hello world 2");
+  }
+})
+
 
 app.get('dist/vendor/react-with-jsxtransformer.min.js', function(req, res) {
   res.sendFile('./dist/vendor/react-with-jsxtransformer.min.js');

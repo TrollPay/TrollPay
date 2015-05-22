2aa2a2e1809e97c4023e3775ed3ef9a78511750f
Pavan Ravipati <psravipati@gmail.com>
2015-05-22 10:53:52 -0700

    Get JSON back from Venmo API

--- server/server.js
+++ server/server.js
@@ -5,6 +5,15 @@
 var express = require('express');
 var path = require('path');
 var api = require('./config.js');
+var utils = require('./utils.js');
+
+/* *************************
+ * Get API secret & app id *
+ * ************************/
+
+
+var api_secret = process.env.API_SECRET || api.secret;
+var app_id = process.env.APP_ID || api.id;
 
 /* ***********************
  * Initialize Middleware *
@@ -23,16 +32,19 @@ var app = express();
 
 // index.html
 app.get('/pay', function(req, res) {
-  var url = "https://api.venmo.com/v1/oauth/authorize?client_id="+api.id+"&scope=make_payments%20access_profile%20access_email%20access_phone%20access_balance&response_type=code";
+  var url = "https://api.venmo.com/v1/oauth/authorize?client_id="+app_id+"&scope=make_payments%20access_profile%20access_email%20access_phone%20access_balance&response_type=code";
   res.redirect(url);
 });
 
 app.get('/', function(req, res) {
   if (req.url.search('code') > -1) {
     var auth_code = req.url.substr(7);
-
-    var url = "https://api.venmo.com/v1/oauth/access_token";
-    res.send('<script>alert("Works")</script>');
+    utils.getToken(auth_code, function(body) {
+      var response = body;
+      response_dict = JSON.parse(body);
+      console.log(response_dict.about);
+    });
+    //you could res.send the body outside of the utils function
   }
   else {
     console.log(req.url);
--- server/utils.js
+++ server/utils.js
@@ -1,16 +1,25 @@
-var http = require('http');
+var request = require('request');
 var api = require('./config.js');
 
-function getToken(auth_code) {
-  var post_data = {
-    "client_id": api.id,
-    "client_secret": api.secret,
-    "code": auth_code
-  };
+var api_secret = process.env.API_SECRET || api.secret;
+var app_id = process.env.APP_ID || api.id;
 
-  var post_options = {
-    host: 'https://api.venmo.com/v1/oauth/access_token',
-    method: 'POST'
-  }
-
+function getToken(auth_code, callback) {
+  request.post(
+    'https://api.venmo.com/v1/oauth/access_token',
+    {
+      form: {
+        client_id: app_id,
+        client_secret: api_secret,
+        code: auth_code
+      }
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          callback(body)
+      }
+    }
+  )
 }
+
+module.exports.getToken = getToken;

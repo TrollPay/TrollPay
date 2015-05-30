d20a70e9ad55970569a31245f1989ba39409ceb3
dannydelott <dannydelott@gmail.com>
2015-05-30 01:14:12 -0700

    Add methods and comments to PaymentController
    - Scaffold addNewPayment, processPayments, and cancelPayment
    - Add documentation blocks

--- server/database/payments/PaymentController.js
+++ server/database/payments/PaymentController.js
@@ -1,9 +1,38 @@
 var mongoose = require('mongoose');
+var _ = require('underscore');
 var paymentSchema = require('./PaymentSchema.js');
 
-module.exports.Payment = mongoose.model('Payment', paymentSchema);
+// The Payment model
+var Payment = mongoose.model('Payment', paymentSchema);
 
-module.exports.addNewPayment = function(obj) {
-  var newPayment = new module.exports.Payment(obj);
+/**
+ * addNewPayment creates a new Payment document and adds it to the collection.
+ *
+ * @param {Object} properties An object with all the properties on the schema, except the defaults.
+ *
+ */
+module.exports.addNewPayment = function(properties) {
+
+  // Attach defaults
+  _.extend(properties, {
+    isCancelled: false,
+    created_at: new Date().toISOString()
+  });
+
+  var newPayment = Payment(properties);
   newPayment.save();
+};
+
+/**
+ * processPayments calls the Venmo API on each Payment document in the system
+ */
+module.exports.processPayments = function() {
+
+};
+
+/**
+ * cancelPayment voids a Payment by setting its isCancelled property to TRUE
+ */
+module.exports.cancelPayment = function(id) {
+
 };

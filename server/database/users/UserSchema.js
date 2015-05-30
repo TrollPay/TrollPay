dfbda106157e8f24eaa29dc4c67e61805ad4a38a
dannydelott <dannydelott@gmail.com>
2015-05-30 01:15:13 -0700

    Add comments

--- server/database/users/UserSchema.js
+++ server/database/users/UserSchema.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
-  user_id: String,  // encrypted
+  user_id: String, // encrypted
   access_token: String,
   refresh_token: String,
   first_name: String,
@@ -12,5 +12,6 @@ module.exports = new mongoose.Schema({
   email: String,
   phone: String,
   profile_picture_url: String,
-  date_joined: Date
+  date_joined: Date,
+  payments: [String] // array of Payment ids
 });

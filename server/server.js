e93d9e7ba3c9000d2e7cd724b700d3fe72495469
dannydelott <dannydelott@gmail.com>
2015-05-30 14:43:17 -0700

    Add Promises, comments, and test route
    - Promisify the PaymentController
    - Add a new Payment and cancel a Payment in /test route

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
+// NPM Modules
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
+var Promises = require('bluebird');
 
+// Local Modules
 var api = require('./config.js');
 var utils = require('./utils.js');
 var PaymentController = require('./database/payments/PaymentController.js');
 
-// Get API secret & app id
+// Promisify Controller
+Promises.promisifyAll(PaymentController);
 
+// Get API secret & app id
 var api_secret = process.env.API_SECRET || api.secret;
 var app_id = process.env.APP_ID || api.id;
 var mongo_host = process.env.MONGO_HOST || 'mongodb://localhost/test';
 
 // Instantiate the express object
-
 var app = express();
 
 // Connect to MongoDB
@@ -28,7 +32,28 @@ db.once('open', function() {
 // app.use(express.static(path.resolve("./client")));
 
 // GET Requests
-app.get('/test', function(req, res) {});
+app.get('/test', function(req, res) {
+  console.log(req.url);
+
+  // test adding the new payment asynchronously
+  PaymentController.addNewPaymentAsync({
+      sender_id: 'senderid',
+      recipient_email: 'test@test.com',
+      note: 'This is a test',
+      total: 1.50
+    })
+    .then(function(payment) {
+      res.send('New Payment ID: ' + payment._id);
+    })
+    .catch(function(err) {});
+
+  // test cancelling a payment asynchronously
+  PaymentController.cancelPaymentAsync('556a2ceb2a507b0e0ff73cab')
+    .then(function(result) {
+      console.log('Cancelled', result.nModified, 'payments');
+    })
+    .catch(function(err) {});
+});
 
 app.get('/pay', function(req, res) {
   var url = "https://api.venmo.com/v1/oauth/authorize?client_id=" + app_id + "&scope=make_payments%20access_profile%20access_email%20access_phone%20access_balance&response_type=code";
@@ -40,7 +65,9 @@ app.post('/Users', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-  if(req.url.search('code') > -1) {
+  console.log(req.url);
+
+  if (req.url.search('code') > -1) {
     var auth_code = req.url.substr(7);
     utils.getToken(auth_code, function(body) {
       var response = body;
@@ -50,8 +77,7 @@ app.get('/', function(req, res) {
     //you could res.send the body outside of the utils function
     //TODO: Import our mongo database and use it here to store our initial data about the user
   } else {
-    console.log(req.url);
-    res.send("hello world 2");
+    res.send('Hello World');
   }
 });
 
@@ -64,7 +90,6 @@ app.get('css/style.css', function(req, res) {
 });
 
 // Start the server
-
 var port = process.env.PORT || 3000;
 
 var server = app.listen(port, function() {

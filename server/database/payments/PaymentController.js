74da4532a9c15b1ffb2070299ab111c240a98043
dannydelott <dannydelott@gmail.com>
2015-05-30 14:37:51 -0700

    Create addNewPayment and cancelPayment
    - Build out the methods for adding and cancelling payments

--- server/database/payments/PaymentController.js
+++ server/database/payments/PaymentController.js
@@ -9,30 +9,62 @@ var Payment = mongoose.model('Payment', paymentSchema);
  * addNewPayment creates a new Payment document and adds it to the collection.
  *
  * @param {Object} properties An object with all the properties on the schema, except the defaults.
- *
  */
-module.exports.addNewPayment = function(properties) {
+module.exports.addNewPayment = function(properties, callback) {
 
   // Attach defaults
+  // TODO: DO THIS CLIENT SIDE
   _.extend(properties, {
     isCancelled: false,
-    created_at: new Date().toISOString()
+    created_at: new Date().toISOString(),
+    balance: properties.total,
+    installments: []
   });
 
+  // create new Payment document
   var newPayment = Payment(properties);
-  newPayment.save();
+
+  // Saves the Payment document
+  newPayment.save(function(err, payment) {
+    if (err) {
+      console.log('ERROR: Could not add new payment');
+      callback(true, err);
+    } else {
+      console.log('SUCCESS: Payment added');
+      callback(false, payment);
+    }
+  });
 };
 
 /**
  * processPayments calls the Venmo API on each Payment document in the system
  */
-module.exports.processPayments = function() {
+module.exports.processPayments = function(callback) {
 
 };
 
 /**
  * cancelPayment voids a Payment by setting its isCancelled property to TRUE
+ *
+ * @param {Number} id Payment ID
  */
-module.exports.cancelPayment = function(id) {
+module.exports.cancelPayment = function(id, callback) {
+
+  Payment.update({
+    '_id': id
+  }, {
+    'isCancelled': true
+  }, function(err, result) {
+    if (err) {
+      console.log('ERROR: Could not cancel payment');
+      callback(true, err);
+    } else if (result.nModified < 1) {
+      console.log('WARNING: Payment is already cancelled');
+      callback(false, result);
+    } else {
+      console.log('SUCCESS: Payment cancelled');
+      callback(false, result);
+    }
+  });
 
 };

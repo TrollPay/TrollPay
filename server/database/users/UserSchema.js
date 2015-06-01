0993197ea9f221dbad27f7dba6f05ced38d44e63
dannydelott <dannydelott@gmail.com>
2015-06-01 16:56:07 -0700

    Update/Remove schema properties

--- server/database/users/UserSchema.js
+++ server/database/users/UserSchema.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
-  user_id: String, // encrypted
+  venmo_id: String, // encrypted
   access_token: String,
   refresh_token: String,
   first_name: String,
@@ -13,5 +13,5 @@ module.exports = new mongoose.Schema({
   phone: String,
   profile_picture_url: String,
   date_joined: Date,
-  payments: [String] // array of Payment ids
+  ip_log: String
 });

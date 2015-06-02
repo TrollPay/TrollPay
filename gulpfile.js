ba9f81cc68adc6aff5f46b49bb88e8f23267f6f9
Pavan Ravipati <psravipati@gmail.com>
2015-06-01 17:01:28 -0700

    Add bootstrap to front end. Closes issue #13

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var gulp = require('gulp'),
   plumber = require('gulp-plumber'),
   uglify = require('gulp-uglify'),
   concat = require('gulp-concat'),
-  nodemon = require('gulp-nodemon');
+  nodemon = require('gulp-nodemon'),
+  minifyCss = require('gulp-minify-css');
 
 /* ************
  * FILE PATHS *
@@ -19,6 +20,8 @@ var path = {};
 path.BOWER_COMPONENTS_DIR = './bower_components/';
 
 /* Client */
+
+//React
 path.CLIENT_DIR = './client/';
 path.DIST_DIR = path.CLIENT_DIR + './dist/';
 path.VENDOR_DIR = path.DIST_DIR + 'vendor/';
@@ -31,6 +34,23 @@ path.REACT_SRC = [
   path.REACT_DIR + 'JSXTransformer.js'
 ];
 path.REACT_MIN_SRC = 'react-with-jsxtransformer.min.js';
+
+//Boostrap
+path.BOOTSTRAP_DIR = path.BOWER_COMPONENTS_DIR + 'bootstrap/';
+path.BOOTSTRAP_JS_DIR = path.BOOTSTRAP_DIR + '/dist/js/';
+path.BOOTSTRAP_CSS_DIR = path.BOOTSTRAP_DIR + '/dist/css/';
+
+path.BOOTSTRAP_JS_SRC = [
+  path.BOOTSTRAP_JS_DIR + 'bootstrap.js',
+  path.BOOTSTRAP_JS_DIR + 'npm.js'
+];
+path.BOOTSTRAP_JS_MIN_SRC = 'bootstrap-with-npm.min.js';
+
+path.BOOTSTRAP_CSS_SRC = [
+  path.BOOTSTRAP_CSS_DIR + 'bootstrap-theme.css',
+  path.BOOTSTRAP_CSS_DIR + 'bootstrap.css'
+]
+path.BOOTSTRAP_CSS_MIN_SRC = 'bootstrap.min.css'
 
 /* Server */
 path.SERVER_DIR = './server/';
@@ -61,7 +81,22 @@ gulp.task('vendor', function() {
     .pipe(concat(path.REACT_MIN_SRC))
     .pipe(gulp.dest(path.VENDOR_DIR));
 
+  //Bootstrap JS
+  gulp.src(path.BOOTSTRAP_JS_SRC)
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(concat(path.BOOTSTRAP_JS_MIN_SRC))
+    .pipe(gulp.dest(path.VENDOR_DIR));
+
+  //BOOTSTRAP CSS
+  gulp.src(path.BOOTSTRAP_CSS_SRC)
+    .pipe(plumber())
+    .pipe(minifyCss())
+    .pipe(concat(path.BOOTSTRAP_CSS_MIN_SRC))
+    .pipe(gulp.dest(path.VENDOR_DIR));
+
 });
+
 
 /* Start the server with nodemon */
 gulp.task('nodemon', function() {
--- server/server.js
+++ server/server.js
@@ -26,17 +26,51 @@ db.once('open', function() {
 var app = express();
 
 // Use the static assets from the client directory
-// app.use(express.static(path.resolve("./client")));
+app.use(express.static(path.resolve("../client")));
 
 // GET Requests
 app.get('/confirm', routes.confirm);
 app.get('/login', routes.login);
 app.get('/', function(req, res) {
-  res.send('Hello World');
+
+  // Check cookie for current user
+  // decrypt ID to see if user exists in collection
+  //
+  // user exists:
+  // redirect to: /dashboard
+  // else
+  // show form
+
+  console.log(req.url);
+
+  if (req.url.search('code') > -1) {
+    var auth_code = req.url.substr(7);
+    utils.getToken(auth_code, function(body) {
+      var response = body;
+      response_dict = JSON.parse(body);
+      console.log(response_dict);
+      res.send("<img src=" + response_dict.user.profile_picture_url + " />");
+
+    });
+    //you could res.send the body outside of the utils function
+    //TODO: Import our mongo database and use it here to store our initial data about the user
+  } else {
+    res.sendFile('index.html', { root: path.join(__dirname, '../client') });
+  }
+
 });
 
 app.get('dist/vendor/react-with-jsxtransformer.min.js', function(req, res) {
   res.sendFile('./dist/vendor/react-with-jsxtransformer.min.js');
+});
+
+// app.get('dist/vendor/bootstrap-with-npm.min.js', function(req, res) {
+//   res.sendFile('./dist/vendor/bootstrap-with-npm.min.js');
+// });
+
+app.get('/dist/vendor/bootstrap.min.css', function(req, res) {
+  console.log('in here')
+  res.sendFile(path.join(__dirname, '../client/dist/vendor','bootstrap.min.css'));
 });
 
 app.get('css/style.css', function(req, res) {

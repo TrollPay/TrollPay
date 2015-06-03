c14b20d137483f4a2db2d830abd94fa45c66e309
Pavan Ravipati <psravipati@gmail.com>
2015-06-03 01:03:50 -0700

    Add functionality to signup form

--- client/build/app.jsx
+++ client/build/app.jsx
@@ -1,13 +1,26 @@
-var App = React.createClass({
+var Signup = React.createClass({
+  invalidSubmit: function() {
+    //TODO: notify user that form was improperly submitted and ask to try again
+  },
+  storeData: function() {
+    //TODO: store the user's data in localStorage
+  },
   handleSubmit: function(e) {
-    console.log(e.target);
+    e.preventDefault();
+    var inputs = Array.prototype.slice.call(e.target);
+    inputs.forEach(function(input) {
+      if (input.value) {
+        console.log(input.attributes.placeholder);
+        input.value = '';
+      }
+    });
   },
   render: function() {
     return (
-      <form onsubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <FormInput tag="recipient" placeholder="Recipient's email"/>
         <FormInput tag="note" placeholder="Optional note"/>
-        <FormInput tag="total"placeholder="$Total Amount"/>
+        <FormInput tag="total" placeholder="$Total Amount"/>
         <input type="submit" onsubmit={this.handleSubmit}/>
       </form>
     );
@@ -15,7 +28,7 @@ var App = React.createClass({
 });
 
 React.render(
-  <App />,
+  <Signup />,
   document.getElementById('app')
 );
 

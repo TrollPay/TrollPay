0b2912ac37263e419c204f2e3913cf7eab6aaad6
Pavan <poppin3000@Pavans-MacBook-Pro-2.local>
2015-06-06 13:57:18 -0700

    Implement react router structure in app jsx with default signup route

--- client/build/app.jsx
+++ client/build/app.jsx
@@ -1,17 +1,28 @@
 //App.jsx should be our main mounting point for react router
 //TODO: Add gulp min / uglify task to all react build code
 
+Router = ReactRouter;
+
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
 var App = React.createClass({
   render: function() {
     return (
-      <Signup />
+      <RouteHandler />
     );
   }
 });
 
-
-React.render(
-  <App />,
-  document.getElementById('app')
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <DefaultRoute handler={Signup} />
+  </Route>
 );
+
+Router.run(routes, function(Handler) {
+  React.render(<Handler />, document.getElementById('app'));
+});
 

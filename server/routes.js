238b5963085dae6d32483a0fdb0f108f3918fd9c
dannydelott <dannydelott@gmail.com>
2015-07-14 23:23:42 -0700

    Update createPayment promises

--- server/routes.js
+++ server/routes.js
@@ -1,75 +1,63 @@
-//Default Modules
+// Default Modules
 var Url = require('url');
 var https = require('https');
-var needle = require('needle');
+var Promise = require('bluebird');
 
 // Local Modules
 var routes = require('./routes.js');
 var PaymentController = require('./database/payments/PaymentController.js');
 var UserController = require('./database/users/UserController.js');
 var UserUtils = require('./database/users/UserUtils.js');
-var Promise = require('bluebird');
-
-// Set environment variables
-var api_secret = process.env.API_SECRET;
-var app_id = process.env.APP_ID;
-
-module.exports.fetchUser = function(code) {
-  return new Promise(function(resolve, reject) {
-    var url = "https://api.venmo.com/v1/oauth/access_token";
-    var data = {
-      "client_id": app_id,
-      "client_secret": api_secret,
-      "code": code
-    };
-    needle.post(url, data,
-      function(err, resp, body) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve({
-            user: body.user,
-            access_token: body.access_token,
-            refresh_token: body.refresh_token
-              //ip: ''
-          });
-        }
-      });
-  });
-
-};
 
 module.exports.createPayment = function(req, res) {
-  module.exports.fetchUser(req.body.code)
-    .then(UserController.createNewUser)
-    .then(UserController.insertNewUser)
-    .then(function(mongoUser) {
-      console.log('user created/updated', mongoUser);
+
+  // Parses values from request body
+  var payment = req.body.payment;
+  var code = req.body.code;
+
+  // logs the ip and reason
+  var ip = _makeIPLog(req.connection.remoteAddress, 'createPayment');
+
+  // stores the updated venmo credentials
+  var venmo = null;
+
+  UserController.fetchUserFromVenmo(code)
+    .then(_lookupUserByVenmo)
+    .then(_updateUserVenmoDetails)
+    .then(_createPayment)
+    .then(PaymentController.addNewPayment)
+    .then(function() {
+      res.send('Done');
     })
     .catch(function(error) {
       console.log(error);
     });
-};
 
-module.exports.confirm = function(req, res) {
-  console.log('on the confirm page');
-  var url = Url.parse(req.url, true);
-  var auth_code = url.query.code;
-  if (!auth_code) {
-    console.log('No auth_code provided on url');
-    res.send('Bad Venmo credentials');
-    return;
+  function _lookupUserByVenmo(venmo_data) {
+    venmo = venmo_data;
+    return UserController.getUserByVenmoId(venmo.user.id);
   }
-  var ipAddress = req.connection.remoteAddress;
 
-  UserUtils.getVenmo(auth_code, ipAddress)
-    .then(UserController.createNewUser)
-    .then(UserController.insertNewUser)
-    .then(function(user) {
-      res.send('Saved Venmo credentials!');
-    })
-    .catch(function(error) {
-      console.log(error);
-      res.send('Bad Venmo credentials');
-    });
+  function _updateUserVenmoDetails(user) {
+    if (!user)
+      user = UserController.createNewUserModel(venmo, ip);
+    else
+      user = UserController.updateUserModel(user, venmo, ip);
+    return UserController.upsertUser(user);
+  }
+
+  function _makeIPLog(ip, reason) {
+    var timestamp = new Date();
+    var log = {
+      timestamp: timestamp.toISOString(),
+      ip: ip,
+      reason: reason
+    };
+    return JSON.stringify(log);
+  }
+
+  function _createPayment(sender_id) {
+    return PaymentController.createNewPaymentModel(payment, sender_id);
+  }
+
 };

50ec88b82f82efac4674ff402a0e682f7949253b
Pavan Ravipati <psravipati@gmail.com>
2015-07-25 17:18:14 -0700

    Update styling and fix url issue. Closes #69

--- client/build/js/dispatcher/dispatcher.js
+++ client/build/js/dispatcher/dispatcher.js
@@ -2,13 +2,13 @@ AppDispatcher.register(function(payload) {
   switch (payload.eventName) {
     case 'new-payment':
       PaymentStore.payment = payload.newPayment;
-      PaymentStore.sendData();
+      PaymentStore.sendData(function() { PaymentStore.trigger('confirmed-payment') })
       break;
 
-    case 'get-user-credentials':
-      UserStore.getUser(payload.authData.code);
-      UserStore.trigger('change');
-      break;
+    // case 'get-user-credentials':
+    //   UserStore.getUser(payload.authData.code);
+    //   UserStore.trigger('change');
+    //   break;
 
     case 'store-form-in-localStorage':
       PaymentStore.putOne(payload.formData.inputs);
--- client/build/js/stores/PaymentStore.js
+++ client/build/js/stores/PaymentStore.js
@@ -1,9 +1,27 @@
+
 var PaymentStore = {
   payment: null,
+  sender: null,
   //function to send data to server
-  sendData: function() {
+  sendData: function(cb) {
     $.post('/payment/create', this.payment, function(data) {
-      console.log(data);
+      console.log('paymentStore recieved the data ' + JSON.stringify(data));
+      //overwrite this.sender with data recieved back from server
+      //UX GOAL: Once the user hits confirm, they reach a confirmation page where they can see their picture,
+
+      this.payment = {
+        total: data.payment.total,
+        recipient_email: data.payment.recipient_email,
+        note: data.payment.note
+      };
+
+      this.sender = {
+        first_name: data.first_name,
+        last_name: data.last_name,
+        profile_pic: data.profile_pic,
+        email: data.email
+      }
+      cb();
     });
 
   },
@@ -23,5 +41,6 @@ var PaymentStore = {
     }.bind(this));
   }
 };
+
 
 MicroEvent.mixin(PaymentStore);

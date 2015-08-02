aa110779245fcba2969cefe4be99a4bf83aa48b1
dannydelott <dannydelott@gmail.com>
2015-08-02 16:59:13 -0700

    Move processPayments to EmailController

--- server/database/emails/EmailController.js
+++ server/database/emails/EmailController.js
@@ -9,3 +9,26 @@ module.exports.sendWelcomeEmails = function(sender, recipient){
   ];
   return Promise.all(emails.map(Utils.sendEmail));
 };
+
+/*
+ * processPayments
+ * Calls processPayment on each payment in the database.
+ */
+module.exports.processPayments = function() {
+  return new Promise(function(resolve, reject) {
+    Payment.find({}, cb);
+    function cb(err, payments) {
+      if (err) { reject(err); }
+      else {
+        payments = payments.filter(filter);
+        payments = payments.map(module.exports.processPayment);
+        resolve(Promise.all(payments));
+       }
+    }
+    function filter(payment){
+      return !(payment.get('cancelled') ||
+        payment.get('untrolled') ||
+        payment.get('trollTolled'));
+    }
+  });
+};

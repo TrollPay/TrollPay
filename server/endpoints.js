4b041b9784ef52ed7cec535068f600253476ba89
dannydelott <dannydelott@gmail.com>
2015-08-04 12:05:56 -0700

    Add TrollPay endpoints

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,2 +1,9 @@
-module.exports.OAUTH = 'https://api.venmo.com/v1/oauth/access_token';
-module.exports.PAYMENTS = 'https://api.venmo.com/v1/payments';
+module.exports.VENMO = {
+  OAUTH: 'https://api.venmo.com/v1/oauth/access_token',
+  PAYMENTS: 'https://api.venmo.com/v1/payments'
+};
+
+module.exports.TROLLPAY = {
+  CLAIM: 'http://trollpay.me/claim/',
+  CANCEL: 'http://trollpay.me/cancel/'
+};

bdce2157e564da1fe3a13db520f0be8d255e9d0b
Pavan Ravipati <psravipati@gmail.com>
2015-08-04 12:39:46 -0700

    commit before changing out demo to jsx format

---
+++ client/build/js/stores/FeedStore.js
@@ -0,0 +1,30 @@
+var FeedStore = {
+  feedPayments: [
+    {
+      payer: "Kendall F",
+      payee: "Mary Margaret H",
+      ago: "1m",
+      profilePic: "./assets/profilepic.jpg"
+    },
+    {
+      payer: "Kendall F",
+      payee: "Mary Margaret H",
+      ago: "1m",
+      profilePic: "./assets/profilepic.jpg"
+    },
+    {
+      payer: "Kendall F",
+      payee: "Mary Margaret H",
+      ago: "1m",
+      profilePic: "./assets/profilepic.jpg"
+    },
+    {
+      payer: "Kendall F",
+      payee: "Mary Margaret H",
+      ago: "1m",
+      profilePic: "./assets/profilepic.jpg"
+    }
+  ]
+}
+
+MicroEvent.mixin(FeedStore); 

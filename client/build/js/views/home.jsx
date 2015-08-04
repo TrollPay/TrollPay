e5482df7bdddaf67b7a6136c5e84b65538f18280
Pavan Ravipati <psravipati@gmail.com>
2015-08-04 12:39:46 -0700

    commit before changing out demo to jsx format

---
+++ client/build/js/views/home.jsx
@@ -0,0 +1,193 @@
+var Home = React.creatClass({
+  render: function() {
+    
+  }
+})
+
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="feed-container">
+        <img id="iphone" src="./assets/iphonemock.png" alt="Iphone" />
+          <div class="iphone-feed">
+            <ul class="feed">
+
+              <!-- Payment feed section  -->
+
+              <div class="feed-header">
+                <div class="toprow">
+                  <img style="width:100%" id="iphone-header" src="./assets/iphone-action-bar.png" />
+                </div>
+              </div>
+
+              <div class="payment-wrapper">
+                <li class="payment">
+                  <div class="payment-top">
+                    <img class="circular" src="./assets/profilepic.jpg" />
+                    <div class="payment-text">
+                      <p>
+                        <span class="names">Kendall F</span> paid 
+                        <span class="names">Mary Margaret H</span>
+                        <span class="ago">1m</span>
+                      </p>
+                      <p>
+                        Pony Farts
+                      </p>
+                      <p>
+                        <span class="like">Like</span>
+                        <span class="comment">Comment</span>
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </div>
+
+              <div class="payment-wrapper">
+                <li class="payment">
+                  <div class="payment-top">
+                    <img class="circular" src="./assets/profilepic.jpg" />
+                    <div class="payment-text">
+                      <p>
+                        <span class="names">Kendall F</span> paid 
+                        <span class="names">Mary Margaret H</span>
+                        <span class="ago">1m</span>
+                      </p>
+                      <p>
+                        Red Darts
+                      </p>
+                      <p>
+                        <span class="like">Like</span>
+                        <span class="comment">Comment</span>
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </div>
+
+              <div class="payment-wrapper">
+                <li class="payment">
+                  <div class="payment-top">
+                    <img class="circular" src="./assets/profilepic.jpg" />
+                    <div class="payment-text">
+                      <p>
+                        <span class="names">Kendall F</span> paid 
+                        <span class="names">Mary Margaret H</span>
+                        <span class="ago">1m</span>
+                      </p>
+                      <p>
+                        Hogwarts
+                      </p>
+                      <p>
+                        <span class="like">Like</span>
+                        <span class="comment">Comment</span>
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </div>
+
+              <div class="payment-wrapper">
+                <li class="payment">
+                  <div class="payment-top">
+                    <img class="circular" src="./assets/profilepic.jpg" />
+                    <div class="payment-text">
+                      <p>
+                        <span class="names">Kendall F</span> paid 
+                        <span class="names">Mary Margaret H</span>
+                        <span class="ago">1m</span>
+                      </p>
+                      <p>
+                        Transbay Bart
+                      </p>
+                      <p>
+                        <span class="like">Like</span>
+                        <span class="comment">Comment</span>
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </div>
+
+              <div class="payment-wrapper">
+                <li class="payment">
+                  <div class="payment-top">
+                    <img class="circular" src="./assets/profilepic.jpg" />
+                    <div class="payment-text">
+                      <p>
+                        <span class="names">Kendall F</span> paid 
+                        <span class="names">Mary Margaret H</span>
+                        <span class="ago">1m</span>
+                      </p>
+                      <p>
+                        Go carts 
+                      </p>
+                      <p>
+                        <span class="like">Like</span>
+                        <span class="comment">Comment</span>
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </div>
+
+              <div class="payment-wrapper">
+                <li class="payment">
+                  <div class="payment-top">
+                    <img class="circular" src="./assets/profilepic.jpg" />
+                    <div class="payment-text">
+                      <p>
+                        <span class="names">Kendall F</span> paid 
+                        <span class="names">Mary Margaret H</span>
+                        <span class="ago">1m</span>
+                      </p>
+                      <p>
+                        Go carts 
+                      </p>
+                      <p>
+                        <span class="like">Like</span>
+                        <span class="comment">Comment</span>
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </div>
+
+              <div class="payment-wrapper">
+                <li class="payment">
+                  <div class="payment-top">
+                    <img class="circular" src="./assets/profilepic.jpg" />
+                    <div class="payment-text">
+                      <p>
+                        <span class="names">Kendall F</span> paid 
+                        <span class="names">Mary Margaret H</span>
+                        <span class="ago">1m</span>
+                      </p>
+                      <p>
+                        Go carts 
+                      </p>
+                      <p>
+                        <span class="like">Like</span>
+                        <span class="comment">Comment</span>
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              </div>
+
+              <!-- End Payment feed section -->
+
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div id="app"></div>
+        <div id="pitch" class="text-center">
+          <h3>How TrollPay Works</h3>
+          <ul style="list-style-type: none;">
+            <li>Asymmetrical tilde trust fund art party, DIY slow-carb Williamsburg stumptown.</li>
+            <li>Cred freegan narwhal listicle, Wes Anderson yr pug semiotics single-origin</li>
+            <li>Normcore wayfarers mumblecore sriracha.Salvia blog meditation, post-ironic vegan</li>
+          </ul>
+        </div>
+      </div>
+    </div>

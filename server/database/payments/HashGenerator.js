67e1ee2027d715a7678c88105d36c924fe23070a
dannydelott <dannydelott@gmail.com>
2015-08-04 12:03:23 -0700

    Fix bugs in HashGenerator lookups

--- server/database/payments/HashGenerator.js
+++ server/database/payments/HashGenerator.js
@@ -1,15 +1,19 @@
+/****************************** CONSTANT TYPES ********************************/
+
 var NUM_ROUNDS = 10;
 var SECRET = {
   CANCEL: process.env.CANCEL,
-  TROLLTOLL: process.env.TROLLTOLL,
-  UNTROLL: process.env.UNTROLL,
   CLAIM: process.env.CLAIM
 };
+
+/******************************** NPM MODULES *********************************/
 
 var Promise = require('bluebird');
 var _ = require('underscore');
 var bcrypt = Promise.promisifyAll(require('bcrypt'));
 var Base64 = require('js-base64').Base64;
+
+/****************************** PUBLIC METHODS ********************************/
 
 module.exports.generateHashes = function(id, num_claims){
   return new Promise(function(resolve, reject){
@@ -17,15 +21,11 @@ module.exports.generateHashes = function(id, num_claims){
     var promises = [];
 
     promises.push(generateCancel(id).then(setCancel));
-    promises.push(generateTrollToll(id).then(setTrollToll));
-    promises.push(generateUntroll(id).then(setUntroll));
     promises.push(generateClaims(id, num_claims).then(setClaims));
 
     Promise.all(promises).then(function(array){ resolve(hashes); });
 
     function setCancel(cancel){ hashes.cancel = cancel; }
-    function setTrollToll(trolltoll){ hashes.trolltoll = trolltoll; }
-    function setUntroll(untroll){ hashes.untroll = untroll; }
     function setClaims(claims){ hashes.claims = claims; }
   });
 };
@@ -43,16 +43,10 @@ module.exports.decodeBase64 = function(encoded){
   return Base64.decode(encoded).split(':');
 };
 
+/****************************** PRIVATE METHODS *******************************/
+
 var generateCancel = function(id){
   return bcrypt.hashAsync(SECRET.CANCEL + id, NUM_ROUNDS);
-};
-
-var generateTrollToll = function(id){
-  return bcrypt.hashAsync(SECRET.TROLL_TOLL + id, NUM_ROUNDS);
-};
-
-var generateUntroll = function(id){
-  return bcrypt.hashAsync(SECRET.UNTROLL + id, NUM_ROUNDS);
 };
 
 var generateClaims = function(id, num){
